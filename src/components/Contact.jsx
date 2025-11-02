32a7903783d70db0a290eb4d9a48a15b777c7a19
xDrais <yassin001009@gmail.com>
2025-11-02 19:33:08 +0100

    Updated profile and navbar

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -68,7 +68,7 @@ const Contact = () => {
     <div
       className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
     >
-      <motion.div
+      {/* <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
         className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
       >
@@ -121,7 +121,7 @@ const Contact = () => {
             {loading ? "Sending..." : "Send"}
           </button>
         </form>
-      </motion.div>
+      </motion.div> */}
 
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
