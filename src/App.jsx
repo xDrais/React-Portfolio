b619bb17e9e6df42c0924874a42296be872c543d
xDrais <yassin001009@gmail.com>
2025-11-03 19:49:20 +0100

    Added new canvas and updated overview canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, ThreshCanvas } from "./components";
 
 const App = () => {
   return (
@@ -18,8 +18,10 @@ const App = () => {
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
+          {/* <ThreshCanvas/> */}
         </div>
       </div>
+      
     </BrowserRouter>
   );
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,8 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  // const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./msidesktop/scene.gltf");
 
   return (
     <mesh>
@@ -21,10 +22,16 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
+        scale={isMobile ? 5 : 10}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, 0]}
+      />
+      {/* <primitive
+        object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
-      />
+      /> */}
     </mesh>
   );
 };
